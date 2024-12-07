0087e95522e37d792aff245946c78b7deebb0b3f
Ethan D. Twardy <ethan.twardy@gmail.com>
2024-12-07 12:21:59 -0600

    Parse a boot loader entry file to get the configuration

--- instant-netboot/src/main.rs
+++ instant-netboot/src/main.rs
@@ -1,67 +1,35 @@
-use std::{net::SocketAddr, path::Path};
+use std::{fs, path::PathBuf, str::FromStr};
 
 use async_std::task::block_on;
-use async_tftp::{packet, server::TftpServerBuilder};
-use boot_loader_entries::{BootEntry, EntryKey};
-use futures::AsyncRead;
+use async_tftp::server::TftpServerBuilder;
+use boot_loader_entries::BootEntry;
+use clap::Parser;
 use instant_netboot::NetbootServer;
 use tracing::info;
 
 mod instant_netboot;
+mod tftp;
 
-/// Adapter for async_tftp
-struct TftpHandler {
-    server: NetbootServer,
-}
-
-impl From<instant_netboot::Error> for packet::Error {
-    fn from(value: instant_netboot::Error) -> Self {
-        match value {
-            instant_netboot::Error::InvalidRequestPath => {
-                packet::Error::Msg("Failed to parse request path".to_string())
-            }
-            instant_netboot::Error::FileNotFound => packet::Error::FileNotFound,
-            instant_netboot::Error::IoError => packet::Error::Msg("I/O error".to_string()),
-        }
-    }
-}
-
-#[async_trait::async_trait]
-impl async_tftp::server::Handler for TftpHandler {
-    type Reader = Box<dyn AsyncRead + Send + Unpin + 'static>;
-    type Writer = futures::io::Sink;
-
-    async fn read_req_open(
-        &mut self,
-        _client: &SocketAddr,
-        path: &Path,
-    ) -> Result<(Self::Reader, Option<u64>), packet::Error> {
-        Ok((self.server.tftp_get(path).await?, None))
-    }
-
-    async fn write_req_open(
-        &mut self,
-        _client: &SocketAddr,
-        _path: &Path,
-        _size: Option<u64>,
-    ) -> Result<Self::Writer, packet::Error> {
-        Err(packet::Error::IllegalOperation)
-    }
+#[derive(clap::Parser)]
+struct Args {
+    /// The configuration file
+    pub configuration: PathBuf,
 }
 
 fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
         .with_writer(std::io::stderr)
         .init();
 
-    let configuration = BootEntry {
-        keys: vec![EntryKey::Linux("stereo-gadget/Image".into())],
-    };
+    info!("Serving configuration: {}", args.configuration.display());
+    let configuration = BootEntry::from_str(&String::from_utf8(fs::read(args.configuration)?)?)?;
     let server = NetbootServer::new(configuration);
     block_on(async {
         let addr = "[::1]:6969".parse()?;
-        let tftpd = TftpServerBuilder::with_handler(TftpHandler { server })
+        let tftpd = TftpServerBuilder::with_handler(tftp::TftpHandler { server })
             .bind(addr)
             .build()
             .await?;
---
+++ instant-netboot/src/tftp.rs
@@ -0,0 +1,46 @@
+use std::{net::SocketAddr, path::Path};
+
+use async_tftp::packet;
+use futures::AsyncRead;
+
+use crate::instant_netboot;
+
+/// Adapter for async_tftp
+pub(crate) struct TftpHandler {
+    pub server: instant_netboot::NetbootServer,
+}
+
+impl From<instant_netboot::Error> for packet::Error {
+    fn from(value: instant_netboot::Error) -> Self {
+        match value {
+            instant_netboot::Error::InvalidRequestPath => {
+                packet::Error::Msg("Failed to parse request path".to_string())
+            }
+            instant_netboot::Error::FileNotFound => packet::Error::FileNotFound,
+            instant_netboot::Error::IoError => packet::Error::Msg("I/O error".to_string()),
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl async_tftp::server::Handler for TftpHandler {
+    type Reader = Box<dyn AsyncRead + Send + Unpin + 'static>;
+    type Writer = futures::io::Sink;
+
+    async fn read_req_open(
+        &mut self,
+        _client: &SocketAddr,
+        path: &Path,
+    ) -> Result<(Self::Reader, Option<u64>), packet::Error> {
+        Ok((self.server.tftp_get(path).await?, None))
+    }
+
+    async fn write_req_open(
+        &mut self,
+        _client: &SocketAddr,
+        _path: &Path,
+        _size: Option<u64>,
+    ) -> Result<Self::Writer, packet::Error> {
+        Err(packet::Error::IllegalOperation)
+    }
+}
